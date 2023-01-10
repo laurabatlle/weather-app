8db9a966650baaf53a9e198bd5443e15f4109446
laurabatlle <laura.batlle007@gmail.com>
2023-01-10 13:56:57 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -30,70 +30,72 @@ function showWeather(response) {
   );
 }
 
-function foreCasttoday (response) {
-document.querySelector("#temp-max").innerHTML = Math.round(
-  response.data.daily.temperature.maximum
-);
-document.querySelector("#temp-min").innerHTML = Math.round(
-  response.data.daily.temperature.minimum
-);
+function foreCasttoday(response) {
+  document.querySelector("#temp-max").innerHTML = Math.round(
+    response.data.daily.temperature.maximum
+  );
+  document.querySelector("#temp-min").innerHTML = Math.round(
+    response.data.daily.temperature.minimum
+  );
 
+  function searchCity(city) {
+    let apiKey = "6b72d3t9e0a187fb46324o57dba90ad0";
+    let apiUrl =
+      "https://api.shecodes.io/weather/v1/current?query={city}&key={apiKey}&units=metric";
+    axios.get(apiUrl).then(showWeather);
+  }
 
-function searchCity(city) {
-  let apiKey = "6b72d3t9e0a187fb46324o57dba90ad0";
-  let apiUrl = "https://api.shecodes.io/weather/v1/current?query={city}&key={apiKey}&units=metric";
-  axios.get(apiUrl).then(showWeather);
+  function searchCityForecast(city) {
+    let apiKey = "6b72d3t9e0a187fb46324o57dba90ad0";
+    let apiUrl =
+      "https://api.shecodes.io/weather/v1/forecast?query={query}&key={key}&units=metric";
+    axios.get(apiUrl).then(foreCasttoday);
+  }
+
+  function showCity(event) {
+    event.preventDefault();
+    let city = document.querySelector("#input-city").value;
+    searchCity(city);
+  }
+
+  let searchCityForm = document.querySelector("#search-city");
+  searchCityForm.addEventListener("submit", showCity);
+
+  //
+
+  let currentLocationButton = document.querySelector("#current-position");
+  currentLocationButton.addEventListener("click", getCurrentLocation);
+  searchCity("BARCELONA");
+
+  function getPosition(position) {
+    let apiKey = "6b72d3t9e0a187fb46324o57dba90ad0";
+    let lat = position.coordinates.latitude;
+    let lon = position.coordinates.longitude;
+    let url = `https://api.shecodes.io/weather/v1/current?lon={lon}&lat={lat}&key={apiKey}&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+
+  function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(getPosition);
+  }
+
+  //
+  function changeFarenheit(event) {
+    event.preventDefault();
+    let changeDegrees = document.querySelector("#temperature-element");
+    changeDegrees.innerHTML = `23`;
+  }
+
+  function changeCelsius(event) {
+    event.preventDefault();
+    let changeDegrees = document.querySelector("#temperature-element");
+    changeDegrees.innerHTML = `19`;
+  }
+
+  let convertFarenheit = document.querySelector("#farenheit-degrees");
+  convertFarenheit.addEventListener("click", changeFarenheit);
+
+  let convertCelsius = document.querySelector("#celsius-degrees");
+  convertCelsius.addEventListener("click", changeCelsius);
 }
-
-function searchCityForecast(city) {
-  let apiKey = "6b72d3t9e0a187fb46324o57dba90ad0";
-  let apiUrl = "https://api.shecodes.io/weather/v1/forecast?query={query}&key={key}&units=metric";
-  axios.get(apiUrl).then(foreCasttoday);
-}
-
-function showCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#input-city").value;
-  searchCity(city);
-}
-
-let searchCityForm = document.querySelector("#search-city");
-searchCityForm.addEventListener("submit", showCity);
-
-//
-
-let currentLocationButton = document.querySelector("#current-position");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-searchCity("BARCELONA");
-
-function getPosition(position) {
-  let apiKey = "6b72d3t9e0a187fb46324o57dba90ad0";
-  let lat = position.coordinates.latitude;
-  let lon = position.coordinates.longitude;
-  let url = `https://api.shecodes.io/weather/v1/current?lon={lon}&lat={lat}&key={apiKey}&units=metric`;
-  axios.get(url).then(showWeather);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(getPosition);
-}
-
-//
-function changeFarenheit(event) {
-  event.preventDefault();
-  let changeDegrees = document.querySelector("#temperature-element");
-  changeDegrees.innerHTML = `23`;
-}
-
-function changeCelsius(event) {
-  event.preventDefault();
-  let changeDegrees = document.querySelector("#temperature-element");
-  changeDegrees.innerHTML = `19`;
-}
-
-let convertFarenheit = document.querySelector("#farenheit-degrees");
-convertFarenheit.addEventListener("click", changeFarenheit);
-
-let convertCelsius = document.querySelector("#celsius-degrees");
-convertCelsius.addEventListener("click", changeCelsius);
